b550a68d0ead5c3578dd0109d6a169e181c94958
hinkmond <hinkmond_wong@apple.com>
2020-02-04 16:06:35 -0800

    Merged

--- src/main/java/com/hinkmond/hello/Greeting.java
+++ src/main/java/com/hinkmond/hello/Greeting.java
@@ -6,6 +6,7 @@ public class Greeting {
     private final long id;
     private final String content;
 
+
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
